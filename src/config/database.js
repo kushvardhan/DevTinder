0762ef1e527cd11eba378f44d36ad59fb0f5fb86
Kush Vardhan <kushvardhan39797@gmail.com>
2025-03-03 20:12:20 +0530

    Resolved Request Error in Server

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async ()=>{
-   await mongoose.connect('mongodb://localhost:27017/DevTinder')
+   await mongoose.connect(process.env.MONGO_URI);
 }
 
 module.exports = {connectDB};
