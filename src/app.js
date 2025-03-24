b5a15c36013fba23412f51e097751787c84a0b18
Kush Vardhan <kushvardhan39797@gmail.com>
2025-03-24 19:31:14 +0530

    Added MongoAtlas's cluster URL , removed unnecessary code from app.js and Configured corsOptions

--- src/app.js
+++ src/app.js
@@ -1,31 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const { connectDB } = require('./config/database');
 const { authRouter } = require('./routes/AuthRoute');
 const { profileRouter } = require('./routes/ProfileRoute');
 const { requestRouter } = require('./routes/RequestRouter');
 const { userRouter } = require('./routes/UserRoute');
-const cors = require('cors');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
-
 const corsOptions = {
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST','PATCH', 'DELETE'],  
+  origin: 'http://localhost:5173',  
+  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
+  exposedHeaders: ['set-cookie'],  
 };
-app.use(cors(corsOptions));
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -34,9 +27,6 @@ app.use('/', authRouter);
 app.use('/profile', profileRouter);
 app.use('/request', requestRouter);
 app.use('/user', userRouter);
-
-app.options('*', cors(corsOptions)); 
-
 
 connectDB()
   .then(() => {
